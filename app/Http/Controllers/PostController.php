ea470bf2a46fa1cd619c8660b1f565434c5a7193
cglaz <cezazzr@gmail.com>
2021-01-10 21:30:11 +0100

    Did Post model and the migration

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PostController extends Controller
+{
+    //
+
+    public function show()
+    {
+
+        return view('blog-post');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminsController@index')->name('admin.index');
+Route::get('/post', 'PostController@show')->name('post');
